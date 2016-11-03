e5a4912b37d84bc170fb2928b5b927630652f1fc
lemonade <lemonade@jingdor.com>
2016-11-03 15:31:49 +0800

    update web

--- web/application/controllers/Index.php
+++ web/application/controllers/Index.php
@@ -3,7 +3,7 @@ class IndexController extends BaseController {
 
 	public function indexAction() {  
 		$page=new stdClass;
-		$page->title="页面";
+		$page->title="page";
 		$this->getView()->page =$page;
 		$this->getView()->content = "1st page";
 	}
--- web/function.php
+++ web/function.php
@@ -4,3 +4,7 @@ function layout($content,$page=[],$master="master"){
  	include LAYOUT.'master.phtml';
 	return $content;
 }
+
+function url($path){
+	return  $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].$path;
+}
