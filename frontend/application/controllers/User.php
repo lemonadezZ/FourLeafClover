9f893c0c2224c0cb47dbcc1ef8da3a83ba3941cb
lemonade <lemonade@jingdor.com>
2016-11-06 10:26:17 +0800

    git add user login form

---
+++ frontend/application/controllers/User.php
@@ -0,0 +1,25 @@
+<?php
+class UserController extends BaseController {
+
+	//登录
+	public function loginAction() {
+		$page=new stdClass;
+		$page->title="page";
+		$this->getView()->page =$page;
+		$this->getView()->content = "1st page";
+	}
+	//
+	public function dologinAction() {
+		$page=new stdClass;
+		$page->title="page";
+		$this->getView()->page =$page;
+		$this->getView()->content = "1st page";
+	}
+	//注册
+	public function signupAction() {
+		$page=new stdClass;
+		$page->title="page";
+		$this->getView()->page =$page;
+		$this->getView()->content = "1st page";
+	}
+}
