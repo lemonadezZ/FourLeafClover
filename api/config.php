f853119c3cae032ec16ec3a6c268bebd4e6fd182
lemonade <lemonade@jingdor.com>
2016-11-06 22:38:01 +0800

    m config

--- api/config.php
+++ api/config.php
@@ -1,7 +1,11 @@
 <?php
 return [
-	'debug'=>"false",
+	'debug'=>"false", 
 	'log'=>"error", //debug info notice warning error crit
-	'log_level'=>"info",
-	'doc'=>""
-];
+	'log_level'=>"info", 
+	'db'=>"mysql",  //mysql
+	'db_hostname'=>"127.0.0.1",
+	'db_username'=>"fourleafclover",
+	'db_password'=>"fourleafclover",
+	'db_database'=>"fourleafclover",
+	];
